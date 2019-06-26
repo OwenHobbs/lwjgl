7d00b1719b8c8ae8f737834d22a40c8d8102d464
Owen Hobbs <owenhobbs2006@gmail.com>
2019-06-25 21:19:50 -0500

    VAOs and VBOs

--- Game/src/engineTester/MainGameLoop.java
+++ Game/src/engineTester/MainGameLoop.java
@@ -3,17 +3,39 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				-0.5f, 0.5f, 0f,
+				-.5f, -.5f, 0f,
+				0.5f, -.5f, 0f,
+				.5f, -.5f, 0f,
+				.5f, .5f, 0f,
+				-.5f, .5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		// Game loop
 		while(!Display.isCloseRequested()) {
+			renderer.prepare();
+			
+			renderer.render(model);
+			
 			DisplayManager.updateDisplay();
 		}
-		
+
+		loader.cleanUp();
 		DisplayManager.closeDispaly();
 	}
 	
